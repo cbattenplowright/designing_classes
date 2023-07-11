90682a937b64850f730a216ffb00135fbf4c9e3e
cbattenplowright <callumbp15@gmail.com>
2023-07-11 17:50:15 +0100

    added Computer class and methods

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,2 +1,31 @@
 public class Computer {
+
+    private int currentStorage;
+    private String printerModel;
+
+    public Computer(int storage) {
+        this.currentStorage = storage;
+        this.printerModel = "No printer connected";
+    }
+
+    public int getStorage() {
+        return currentStorage;
+    }
+
+    public String getPrinterModel() {
+        return printerModel;
+    }
+
+    public void setPrinterModel(String printerModel) {
+        this.printerModel = printerModel;
+    }
+
+    public void addStorage(int additionalStorage) {
+        currentStorage += additionalStorage;
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
 }
